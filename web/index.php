17200036c17dc143ca76f331c691b9c075daafa8
Gerli Kõiv <gerliks@gmail.com>
2018-09-10 18:16:20 +0300

    Twig for templating, url for bookings

--- web/index.php
+++ web/index.php
@@ -7,8 +7,18 @@ $app = new Silex\Application();
 // Definitions
 $app['debug'] = true;
 
-$app->get('/hello', function() {
-		return 'Hello world!';
+
+$app->register(new Silex\Provider\TwigServiceProvider(), [
+	
+	'twig.path' => __DIR__.'/../views',
+	
+]);
+
+
+$app->get('/bookings/create', function () use ($app) {
+	
+	return $app['twig']->render('base.html.twig');
+	
 });
 
 $app->run();
