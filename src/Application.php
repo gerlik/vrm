3c16970a3a96856139c0ed967bdec45db1936955
Gerli Kõiv <gerliks@gmail.com>
2018-09-17 13:55:07 +0300

    Created menu, bookings/create needs a fix

--- src/Application.php
+++ src/Application.php
@@ -78,10 +78,15 @@ class Application extends SilexApplication
             $this['db']
         ))
         ->method('GET|POST')
+        ->bind('booking_form')
         ;
-
+        
         $this
-        ->get('/bookings', new ListBookingsController($this['db'], $this['twig']))
+        ->get('/bookings', new ListBookingsController(
+            $this['db'],
+            $this['twig']
+        ))
+        ->bind('booking_list')
         ;
 
     }
